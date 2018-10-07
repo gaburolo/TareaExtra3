50c676be885306c0bcf79a64fad6405c9b210ffb
gaburolo <martinrolo22@hotmail.com>
2018-10-06 19:29:20 -0600

    RSA Y CESAR

---
+++ main.cpp
@@ -0,0 +1,24 @@
+
+//Simple C++ program to encrypt and decrypt a string
+
+#include <iostream>
+#include "Cesar.cpp"
+#include "RSA.cpp"
+
+
+int main()
+{
+
+    //Ingresamos la palabra o frase a encriptar
+    cesar();
+    //Ingresamos el codigo que nos creo
+    cesar();
+
+
+    //Ingresamos los numeros(primos) para encriptar /// Consejo: 7 y 13
+    //palabra o frase que se desea encriptar
+    RSA();
+
+
+    return 0;
+}
